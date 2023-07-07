846a7d43bdb29aa7efff9c3a26978a80eb54d47f
Mary <maryandreeva100@gmail.com>
2023-07-07 15:29:58 +0300

    Add Redux

--- src/Components/Header/Navigation/Category/Category.jsx
+++ src/Components/Header/Navigation/Category/Category.jsx
@@ -1,24 +1,18 @@
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import s from './Category.module.scss';
 import cn from 'classnames';
+import { useSelector } from 'react-redux';
 
 export const Category = ({ list }) => {
-  const location = useLocation().pathname;
-  const genderPath = location.split('/');
-  const gender = genderPath[1] || 'women';
-
-  console.log('list:', list);
-  console.log('location:', location);
-  console.log('gender:', gender);
-
-  const genderObj = list.find((item) => item.link === gender);
+  const gender = useSelector(state => state.navigation.activeGender);
+  const categoriesList = list.find((item) => item.link === gender);
 
   return (
     <ul className={s.category}>
-      {genderObj?.categories.map((item) => {
+      {categoriesList?.categories.map((item) => {
         return (
-          <li key={item.link}>
-            <NavLink className={({isActive}) => cn(s.link, isActive && s.linkActive)} to={`/${gender}/${item.link}`}>
+          <li className={s.item} key={item.link}>
+            <NavLink className={({isActive}) => cn(s.link, isActive && s.linkActive)} to={`${gender}/${item.link}`}>
               {item.title}
             </NavLink>
           </li>
--- src/Components/Header/Navigation/Gender/Gender.jsx
+++ src/Components/Header/Navigation/Gender/Gender.jsx
@@ -1,14 +1,17 @@
 import { NavLink } from 'react-router-dom';
 import s from './Gender.module.scss';
 import cn from 'classnames';
+import { useSelector } from 'react-redux';
 
 export const Gender = ({ list }) => {
+  const gender = useSelector(state => state.navigation.activeGender);
+
   return (
     <ul className={s.gender}>
       {list.map((item) => {
         return (
           <li key={item.link} className={s.item}>
-            <NavLink className={({isActive}) => cn(s.link, isActive && s.linkActive)} to={item.link}>
+            <NavLink className={({isActive}) => cn(s.link, (isActive || gender === item.link) && s.linkActive)} to={item.link}>
               {item.title}
             </NavLink>
           </li>
