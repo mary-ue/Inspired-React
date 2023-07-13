3751174c6325d2cae6377e0612ccac7bcb1a34bc
Mary <maryandreeva100@gmail.com>
2023-07-13 16:11:22 +0300

    Product size

--- src/Components/ProductSize/ProductSize.jsx
+++ src/Components/ProductSize/ProductSize.jsx
@@ -1,13 +1,13 @@
 import s from './ProductSize.module.scss';
 
-export const ProductSize = ({ size }) => {
+export const ProductSize = ({ size, selectedSize, handleSizeChange }) => {
   return (
     <div className={s.size}>
       <p className={s.title}>Размер</p>
       <ul className={s.list}>
-        {size?.map((item, i) => {
-          return (<li key={i} className={s.item}>
-            <input className={s.input} id={item} type="radio" name="size" value={item}  />
+        {size?.map((item) => {
+          return (<li key={item} className={s.item}>
+            <input className={s.input} id={item} type="radio" name="size" value={item} checked={selectedSize === item} onChange={handleSizeChange} />
             <label className={s.check} htmlFor={item}>{item}</label>
           </li>)
         })
