afe9d0fd0d390194c468568cce0cc678a6d1cb49
Mary <maryandreeva100@gmail.com>
2023-07-14 15:27:32 +0300

    Start pagination

---
+++ src/Components/Pagination/Pagination.jsx
@@ -0,0 +1,56 @@
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, useLocation } from "react-router-dom";
+import s from './Paginataion.module.scss';
+import cn from "classnames";
+import { setPage } from "../../features/goodsSlice";
+
+export const Pagination = () => {
+  const pathname = useLocation().pathname;
+  const { page, pages } = useSelector(state => state.goods);
+  const dispatch = useDispatch();
+
+  const handlePageChange = (newPage => {
+    dispatch(setPage(newPage));
+  })
+
+  const handlePrevPage = () => {
+    if (page > 1) {
+      handlePageChange(page - 1);
+    }
+  }
+
+  const handleNextPage = () => {
+    if (page < pages) {
+      handlePageChange(page + 1);
+    }
+  }
+
+  const renderPaginationItems = () => {
+    const paginationItems = [];
+
+    let startPage = Math.max(1, page - 1);
+    let endPage = Math.min(startPage + 2, pages);
+
+    for (let i = startPage; i <= endPage; i++) {
+      paginationItems.push(
+        <li className={s.item} key={i}>
+          <NavLink className={cn(s.link, i === +page ?? s.linkActive)} to={`${pathname}?page=${i}`} onClick={() => handlePageChange(i)}>
+            {i}
+          </NavLink>
+        </li>
+      )
+    }
+
+    return paginationItems;
+  }
+
+  return (
+    <div className={s.pagination}>
+      <button className={s.arrow} onClick={handlePrevPage} disabled={page <= 2}>&lt;</button>
+      <ul className={s.list}>
+        {renderPaginationItems()}
+      </ul>
+      <button className={s.arrow} onClick={handleNextPage} disabled={page >= pages - 1 || pages <= 3}>&gt;</button>
+    </div>
+  )
+}
